4a9352fb6b563d68d3ade9ccf3f5f7f8d97ef72d
xeno <17703938+xenoplayed@users.noreply.github.com>
2025-07-12 12:19:46 +0200

    add leading IconButton to AppBar

--- lib/home.dart
+++ lib/home.dart
@@ -28,6 +28,15 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         // TODO: Add buttons and title (102)
         title: const Text('SHRINE'),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
         // TODO: Add trailing button
       ),
       // TODO: Add a grid view (102)
