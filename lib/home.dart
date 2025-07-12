a29e9fef1a8d9e08bfc456d20841d31473ff4875
xeno <17703938+xenoplayed@users.noreply.github.com>
2025-07-12 12:11:18 +0200

    add AppBar with title

--- lib/home.dart
+++ lib/home.dart
@@ -23,8 +23,13 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
+    return Scaffold(
       // TODO: Add app bar (102)
+      appBar: AppBar(
+        // TODO: Add buttons and title (102)
+        title: const Text('SHRINE'),
+        // TODO: Add trailing button
+      ),
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
