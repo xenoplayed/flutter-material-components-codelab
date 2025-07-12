675a671f0c7f9ae797b5cd372137e95d7f65fc28
xeno <17703938+xenoplayed@users.noreply.github.com>
2025-07-12 11:02:30 +0200

    add CANCEL and NEXT buttons

--- lib/login.dart
+++ lib/login.dart
@@ -64,6 +64,18 @@ class _LoginPageState extends State<LoginPage> {
               // TODO: Add a beveled rectangular border to CANCEL (103)
               children: <Widget>[
                 // TODO: Add buttons (101)
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    // TODO: Clear the text fields (101)
+                  },
+                ),
+                ElevatedButton(
+                  child: const Text('NEXT'),
+                  onPressed: () {
+                    // TODO: Show the next page (101)
+                  },
+                )
               ],
             )
           ],
