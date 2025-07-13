e16ad25adeba98f89084e881129dd56d980c9fbd
xeno <17703938+xenoplayed@users.noreply.github.com>
2025-07-13 19:20:57 +0200

    add purple color

--- lib/colors.dart
+++ lib/colors.dart
@@ -5,6 +5,8 @@ const kShrinePink100 = Color(0xFFFEDBD0);
 const kShrinePink300 = Color(0xFFFBB8AC);
 const kShrinePink400 = Color(0xFFEAA4A4);
 
+const kShrinePurple = Color(0xFF5D1049);
+
 const kShrineBrown900 = Color(0xFF442B2D);
 
 const kShrineErrorRed = Color(0xFFC5032B);
