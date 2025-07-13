a84d1da6d7204fb67de3d3fd2b7b3101698a5c12
xeno <17703938+xenoplayed@users.noreply.github.com>
2025-07-13 18:27:04 +0200

    change textfield shape

--- lib/app.dart
+++ lib/app.dart
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'supplemental/cut_corners_border.dart';
 
 import 'home.dart';
 import 'login.dart';
@@ -58,8 +59,8 @@ ThemeData _buildShrineTheme() {
     ),
     // TODO: Decoarate Inputs (103)
     inputDecorationTheme: const InputDecorationTheme(
-      border: OutlineInputBorder(),
-      focusedBorder: OutlineInputBorder(
+      border: CutCornersBorder(),
+      focusedBorder: CutCornersBorder(
         borderSide: BorderSide(
           width: 2.0,
           color: kShrineBrown900,
