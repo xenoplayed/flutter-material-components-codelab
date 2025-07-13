000963b69f92ce385cc94f2e163352e6aa709647
xeno <17703938+xenoplayed@users.noreply.github.com>
2025-07-13 18:19:35 +0200

    adjust card elevation

--- lib/home.dart
+++ lib/home.dart
@@ -39,6 +39,7 @@ class HomePage extends StatelessWidget {
       return Card(
         clipBehavior: Clip.antiAlias,
         // TODO: Adjust card heights (103)
+        elevation: 0,
         child: Column(
           // TODO: Center items on the card (103)
           crossAxisAlignment: CrossAxisAlignment.start,
