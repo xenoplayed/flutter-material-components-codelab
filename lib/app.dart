4359ef0e78d11e87ab3794baef39dc91b8b5df6d
xeno <17703938+xenoplayed@users.noreply.github.com>
2025-07-13 21:20:47 +0200

    add backdrop to ShrineApp widget

--- lib/app.dart
+++ lib/app.dart
@@ -33,7 +33,16 @@ class ShrineApp extends StatelessWidget {
       routes: {
         '/login': (BuildContext context) => const LoginPage(),
         // TODO: Change to a Backdrop with a HomePage frontLayer (104)
-        '/': (BuildContext context) => const HomePage(),
+        '/': (BuildContext context) => Backdrop(
+              // TODO: Make currentCategory field take _currentCategory (104)
+              currentCategory: Category.all,
+              // TODO: Pass _currentCategory for frontLayer (104)
+              frontLayer: HomePage(),
+              // TODO: Change backLayer field value to CategoryMenuPage (104)
+              backLayer: Container(color: kShrinePink100),
+              frontTitle: Text('SHRINE'),
+              backTitle: Text('MENU'),
+            ),
         // TODO: Make currentCategory field take _currentCategory (104)
         // TODO: Pass _currentCategory for frontLayer (104)
         // TODO: Change backLayer field value to CategoryMenuPage (104)
